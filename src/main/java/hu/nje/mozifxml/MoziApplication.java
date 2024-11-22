cce1dbb05faf1b6f15e1eacb8a878ad3f69d57a2
GHEszti <eszter.otruba@gmail.com>
2024-11-22 10:17:39 +0100

    Párhuzamos programozás menüpont

--- src/main/java/hu/nje/mozifxml/MoziApplication.java
+++ src/main/java/hu/nje/mozifxml/MoziApplication.java
@@ -1,28 +1,33 @@
 package hu.nje.mozifxml;
 
+import hu.nje.mozifxml.controller.MainController;
+import hu.nje.mozifxml.util.Helper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class MoziApplication extends Application {
+    public static final String FXML_VIEW_NAME = "main-view.fxml";
+    public static final String APP_TITLE = "app.name";
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws IOException {
-        final Scene scene = new Scene(
-                FXMLLoader.load(
-                        Objects.requireNonNull(MoziApplication.class.getResource("main-view.fxml"))
-                )
-        );
-        stage.setTitle("Mozi - Java előadás beadandó");
+        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(FXML_VIEW_NAME));
+
+        stage.setTitle(Helper.loadConfigProperties().getProperty(APP_TITLE));
         stage.resizableProperty().setValue(Boolean.FALSE);
-        stage.setScene(scene);
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.setOnCloseRequest(event -> {
+            final MainController controller = fxmlLoader.getController();
+            controller.shutdown();
+        });
         stage.show();
     }
 }
